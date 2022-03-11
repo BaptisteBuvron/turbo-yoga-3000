2998d8ef604e0d470142332d31f3c01d8a62c6d1
Baptiste Buvron <baptiste.buvron@gmail.com>
2022-03-11 10:44:39 -0500

    Animator fix

--- Assets/Trainer.cs
+++ Assets/Trainer.cs
@@ -5,117 +5,134 @@ using UnityEngine.XR;
 
 public class Trainer : MonoBehaviour
 {
-  Animator animator;
-  bool playing = false;
-  string lastAnimation = "Null";
-  // Start is called before the first frame update
+    Animator animator;
+    bool playing = false;
+    string lastAnimation = "Null";
+    // Start is called before the first frame update
 
-  private InputDevice targetDevice;
-  private bool targetFound = false;
-  private bool animationMethodAlreadyCalled = false;
+    private InputDevice targetDevice;
+    private bool targetFound = false;
+    private bool animationMethodAlreadyCalled = false;
 
-  void Start()
-  {
-    animator = GetComponent<Animator>();
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-    if (!targetFound)
+    void Start()
     {
-      List<InputDevice> devices = new List<InputDevice>();
-      InputDeviceCharacteristics requiredCharacteristics = InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right;
-      InputDevices.GetDevicesWithCharacteristics(requiredCharacteristics, devices);
-
-      if (devices.Count > 0)
-      {
-        targetDevice = devices[0];
-        Debug.Log(targetDevice.name + " found");
-        targetFound = true;
-      }
-      else
-      {
-        Debug.Log("No devices found");
-      }
+        animator = GetComponent<Animator>();
     }
 
-    targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool isPrimaryButtonPressed);
-    targetDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool isSecondaryButtonPressed);
-    if (isPrimaryButtonPressed && !playing)
+    // Update is called once per frame
+    void Update()
     {
-      TransitionToAnimation("BrasParDessusTete");
+
+        if (!targetFound)
+        {
+            List<InputDevice> devices = new List<InputDevice>();
+            InputDeviceCharacteristics requiredCharacteristics = InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right;
+            InputDevices.GetDevicesWithCharacteristics(requiredCharacteristics, devices);
+
+            if (devices.Count > 0)
+            {
+                targetDevice = devices[0];
+                Debug.Log(targetDevice.name + " found");
+                targetFound = true;
+            }
+            else
+            {
+                Debug.Log("No devices found");
+            }
+        }
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool isPrimaryButtonPressed);
+        targetDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool isSecondaryButtonPressed);
+        if (isPrimaryButtonPressed && !playing)
+        {
+            TransitionToAnimation("BrasParDessusTete");
+        }
+        else if (isSecondaryButtonPressed && !playing)
+        {
+            TransitionToAnimation("T-pose");
+        }
+        //Debug.Log(isPrimaryButtonPressed + "" + isSecondaryButtonPressed);
     }
-    else if (isSecondaryButtonPressed && !playing)
+
+    public void TransitionToAnimation(string nomAnimation)
     {
-      TransitionToAnimation("T-pose");
-    }
-    //Debug.Log(isPrimaryButtonPressed + "" + isSecondaryButtonPressed);
-  }
+        if (!animationMethodAlreadyCalled)
+        {
+            animationMethodAlreadyCalled = true;
 
-  public void TransitionToAnimation(string nomAnimation)
-  {
-    if (!animationMethodAlreadyCalled)
+            if (lastAnimation != "Null")
+            {
+                StartCoroutine(ReverseLastAnimation(nomAnimation));
+            }
+            else
+            {
+                StartCoroutine(StartAnimationAndWait(nomAnimation));
+            }
+        }
+        else { return; }
+    }
+
+    //   public void StartAnimation(int multiplier, string nomAnimation)
+    //   {
+    //     lastAnimation = multiplier == -1 ? "Null" : nomAnimation;
+    //     animator.SetFloat("forward", multiplier);
+    //     animator.Play(nomAnimation);
+    //   }
+
+    public void TogglePlaying(int fin)
     {
-      animationMethodAlreadyCalled = true;
-
-      if (lastAnimation != "Null")
-      {
-        Debug.Log("Reversing animation " + lastAnimation);
-        StartAnimationAndWait(lastAnimation, true);
-      }
-      Debug.Log("Playing animation " + nomAnimation);
-      StartAnimationAndWait(nomAnimation, false);
-      animationMethodAlreadyCalled = false;
+        playing = !playing;
+        Debug.Log("playing " + playing);
     }
-    else { return; }
-  }
 
-  //   public void StartAnimation(int multiplier, string nomAnimation)
-  //   {
-  //     lastAnimation = multiplier == -1 ? "Null" : nomAnimation;
-  //     animator.SetFloat("forward", multiplier);
-  //     animator.Play(nomAnimation);
-  //   }
+    //   IEnumerator Wait(float duration, string nextAnimation = "Null")
+    //   {
+    //     //This is a coroutine
+    //     Debug.Log("Start Wait() function. The time is: " + Time.time);
+    //     Debug.Log("Float duration = " + duration);
+    //     yield return new WaitForSeconds(duration);   //Wait
+    //     Debug.Log("End Wait() function and the time is: " + Time.time);
+    //     Debug.Log("Now playing animation " + nextAnimation);
+    //     StartAnimation(1, nextAnimation);
+    //     animationMethodAlreadyCalled = false;
+    //   }
 
-  public void TogglePlaying(int fin)
-  {
-    playing = !playing;
-    Debug.Log("playing " + playing);
-  }
-
-  //   IEnumerator Wait(float duration, string nextAnimation = "Null")
-  //   {
-  //     //This is a coroutine
-  //     Debug.Log("Start Wait() function. The time is: " + Time.time);
-  //     Debug.Log("Float duration = " + duration);
-  //     yield return new WaitForSeconds(duration);   //Wait
-  //     Debug.Log("End Wait() function and the time is: " + Time.time);
-  //     Debug.Log("Now playing animation " + nextAnimation);
-  //     StartAnimation(1, nextAnimation);
-  //     animationMethodAlreadyCalled = false;
-  //   }
-
-  IEnumerator StartAnimationAndWait(string animationName, bool reverse = false)
-  {
-    animator.Play(animationName);
-    animator.SetFloat("forward", reverse ? -1 : 1);
-    yield return new WaitForSeconds(getAnimationDuration(animationName));
-  }
-
-  public float getAnimationDuration(string nomAnimation)
-  {
-    AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
-    foreach (AnimationClip clip in clips)
+    IEnumerator ReverseLastAnimation(string nomProchaineAnimation)
     {
-      if (clip.name == nomAnimation)
-      {
-        return clip.length;
-      }
+        animator.SetFloat("forward", -1);
+        animator.Play(lastAnimation);
+        Debug.Log("Reversing " + lastAnimation);
+        yield return new WaitForSeconds(getAnimationDuration(lastAnimation) + 1);
+        Debug.Log("Finished Reversing " + lastAnimation);
+        StartCoroutine(StartAnimationAndWait(nomProchaineAnimation));
+
     }
-    Debug.LogError("Animation " + nomAnimation + " not found");
-    return 10;
-  }
+
+    IEnumerator StartAnimationAndWait(string animationName)
+    {
+        Debug.Log("animation starting");
+        lastAnimation = animationName;
+        animator.SetFloat("forward", 1);
+        animator.Play(animationName);
+
+        yield return new WaitForSeconds(getAnimationDuration(animationName));
+        Debug.Log("animation finished");
+        animationMethodAlreadyCalled = false;
+
+    }
+
+    public float getAnimationDuration(string nomAnimation)
+    {
+        AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
+        foreach (AnimationClip clip in clips)
+        {
+            if (clip.name == nomAnimation)
+            {
+                return clip.length;
+            }
+        }
+        Debug.LogError("Animation " + nomAnimation + " not found");
+        return 10;
+    }
 
 }
