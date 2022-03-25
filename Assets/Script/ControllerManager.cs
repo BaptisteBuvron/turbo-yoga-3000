79aa0f9d6a74c2b584f5b8b0ce54b11c09f184e4
Baptiste Buvron <baptiste.buvron@gmail.com>
2022-03-25 09:10:48 -0400

    CallibrationState

---
+++ Assets/Script/ControllerManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace Script
+{
+    public class ControllerManager : MonoBehaviour
+    {
+        public static InputDevice leftController;
+        public static InputDevice rightController;
+        private bool AlltargetFound = false;
+        
+        private void Update()
+        {
+            if (!AlltargetFound)
+            {
+                List<InputDevice> devicesLeft = new List<InputDevice>();
+                List<InputDevice> devicesRight = new List<InputDevice>();
+                InputDeviceCharacteristics requiredCharacteristicsRight =
+                    InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right;
+                InputDeviceCharacteristics requiredCharacteristicsLeft =
+                    InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Left;
+                InputDevices.GetDevicesWithCharacteristics(requiredCharacteristicsRight, devicesRight);
+                InputDevices.GetDevicesWithCharacteristics(requiredCharacteristicsLeft, devicesLeft);
+
+
+                if (devicesLeft.Count > 0 && devicesRight.Count > 0)
+                {
+                    leftController = devicesLeft[0];
+                    rightController = devicesRight[0];
+                    AlltargetFound = true;
+                    Debug.Log("All target found");
+                }
+                else
+                {
+                    Debug.Log("No devices found");
+                }
+            }
+        }
+    }
+}
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,21 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using Script;
+using TMPro;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class GameManager : MonoBehaviour
 {
 
     public GameState state;
+    public TextMeshProUGUI callibrationText;
+    private float callibrationTime = 0;
+    private InputDevice leftController;
+    private bool leftControllerConnected = false;
+    private bool isPrimaryLeftButtonPressed = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        state = GameState.Menu;
+        state = GameState.Callibrate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    switch (state)
+        {
+            case GameState.Callibrate:
+                if (!leftControllerConnected)    
+                {
+                    getLeftController();
+                }
+                else
+                {
+                    
+                    leftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool isPrimaryLeftButtonPressed);
+                    if (isPrimaryLeftButtonPressed)
+                    {
+                        callibrationTime += Time.deltaTime;
+                        callibrationText.text = "Callibrating...\n" + callibrationTime.ToString("0.00");
+                        if (callibrationTime > 3)
+                        {
+                            callibrationText.text = "";
+                        }
+                    }
+                    
+                }
+                break;
+        }
     }
+
+    private void getLeftController()
+    {
+        leftController = ControllerManager.leftController;
+    }
+        
+    
 }
